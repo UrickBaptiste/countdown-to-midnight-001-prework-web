e579abb2b1c558558b4914b761ed2085c4e97557
UrickBaptiste <urick_baptiste@hotmail.com>
2016-01-03 13:01:20 +0000

    nitrous-backup at Sun Jan  3 13:01:20 UTC 2016

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
+def countdown(number)
+  loop do
+   puts  "#{number} SECONDS(S)!"
+   return "HAPPY NEW YEAR!"
+end
+end
 
-def countdown
+def countdown_with_sleep(seconds)
+
+  while seconds >= 0
+    puts "#{seconds} seconds"
+    seconds -= 1
+    sleep(1)
+end
 end
